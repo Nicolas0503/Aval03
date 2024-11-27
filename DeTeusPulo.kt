c94a4120d8296aec70f8506d8a72859e850492c5
Nícolas Bastos <162383127+Nicolas0503@users.noreply.github.com>
2024-11-27 20:45:54 -0300

    método para decifrar texto

--- DeTeusPulo.kt
+++ DeTeusPulo.kt
@@ -2,3 +2,14 @@ fun main() {
     // Kadu Farias
     val textoCifrado = "^mu({uuxzm0w(w{ƒw(zw„wz(x‚i(uq}+Vmu0{mux‚m(m}0{w}(„ëw(ty~zm(q{{qu3Y}mu0vëw(€mv..."
     }
+  
+//Decifrar texto
+  
+fun decifrarTexto(cifrado: String): String {
+    val chave = { index: Int -> if (index % 5 == 0) 16 else 8 }
+    val textoDecifrado = cifrado.mapIndexed { index, char ->
+        val deslocamento = chave(index)
+        (char.toInt() - deslocamento).toChar()
+    }.joinToString("")
+    return textoDecifrado
+}
