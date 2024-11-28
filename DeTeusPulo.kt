9624c0fc4df3b4d90a44487b63ef82b8a5f0daee
Nícolas Bastos <162383127+Nicolas0503@users.noreply.github.com>
2024-11-27 21:04:41 -0300

    duas funçoes novas

--- DeTeusPulo.kt
+++ DeTeusPulo.kt
@@ -4,7 +4,7 @@ fun main() {
     }
   
 //Decifrar texto
-  
+
 fun decifrarTexto(cifrado: String): String {
     val chave = { index: Int -> if (index % 5 == 0) 16 else 8 }
     val textoDecifrado = cifrado.mapIndexed { index, char ->
@@ -12,4 +12,50 @@ fun decifrarTexto(cifrado: String): String {
         (char.toInt() - deslocamento).toChar()
     }.joinToString("")
     return textoDecifrado
+}
+
+fun substituirPalindromos(texto: String): Pair<String, List<String>> {
+    val palavras = texto.split(Regex("\\s+"))
+    val palindromos = mutableListOf<String>()
+    val palavrasSubstituidas = palavras.map { palavra ->
+        if (palavra.equals(palavra.reversed(), ignoreCase = true) && palavra.length > 1) {
+            palindromos.add(palavra)
+            when (palindromos.size) {
+                1 -> "\u001B[31mcéu\u001B[0m"
+                2 -> "\u001B[31msorri\u001B[0m"
+                3 -> "\u001B[31mvontade\u001B[0m"
+                else -> palavra
+            }
+        } else palavra
+    }
+    return Pair(palavrasSubstituidas.joinToString(" "), palindromos)
+}
+fun substituirPalindromos(texto: String): Pair<String, List<String>> {
+    val palavras = texto.split(Regex("\\s+"))
+    val palindromos = mutableListOf<String>()
+    val palavrasSubstituidas = palavras.map { palavra ->
+        if (palavra.equals(palavra.reversed(), ignoreCase = true) && palavra.length > 1) {
+            palindromos.add(palavra)
+            when (palindromos.size) {
+                1 -> "\u001B[31mcéu\u001B[0m"
+                2 -> "\u001B[31msorri\u001B[0m"
+                3 -> "\u001B[31mvontade\u001B[0m"
+                else -> palavra
+            }
+        } else palavra
+    }
+    return Pair(palavrasSubstituidas.joinToString(" "), palindromos)
+}
+
+fun adicionarNomeParticipantes(
+    texto: String,
+    nome1: String,
+    nome2: String
+): String {
+    val ultimaOcorrencia = texto.lastIndexOf("Vem")
+    return if (ultimaOcorrencia != -1) {
+        val inicio = texto.substring(0, ultimaOcorrencia + 3)
+        val fim = texto.substring(ultimaOcorrencia + 3)
+        "$inicio \u001B[31m$nome1 e $nome2\u001B[0m$fim"
+    } else texto
 }
